769013ad0c17bc10b129b3b37ee36a6b738d397c
Baptiste75-ui <70966657+Baptiste75-ui@users.noreply.github.com>
2021-01-27 15:52:21 +0100

    backbaptiste

--- src/Controller/AbonneController.php
+++ src/Controller/AbonneController.php
@@ -11,9 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface as Encoder;
 
-/**
- * @Route("/admin/abonne")
- */
+
 class AbonneController extends AbstractController
 {
     /**
